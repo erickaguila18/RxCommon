1b836c93b1bd1a9de88a0e3c4414761dadaa28d5
Erick Aguila <erick.aguila18@gmail.com>
2021-03-01 13:39:54 -0600

    Update Versions.kt

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -1,9 +1,9 @@
 object Versions {
-    const val rxcommon = "0.6.1"
-    const val kotlin = "1.3.61"
+    const val rxcommon = "0.7.0"
+    const val kotlin = "1.4.30"
     const val dokka = "0.10.0"
 
     object Android {
-        const val gradleTools = "3.5.0"
+        const val gradleTools = "4.1.2"
     }
 }
