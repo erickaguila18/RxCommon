530ca3b53a4a1ed40dd946459dd6b15aaab37aac
Erick Aguila <erick.aguila18@gmail.com>
2021-03-01 13:40:33 -0600

    Update BuildCacheExtensions.kt

--- buildSrc/src/main/java/BuildCacheExtensions.kt
+++ buildSrc/src/main/java/BuildCacheExtensions.kt
@@ -1,14 +1,13 @@
-import org.gradle.caching.http.HttpBuildCache
+import org.gradle.caching.local.DirectoryBuildCache
 import org.gradle.kotlin.dsl.KotlinSettingsScript
-import java.net.URI
 
 fun KotlinSettingsScript.setupBuildCache() {
     buildCache {
         val localBuildCacheUrl = Properties.gradleLocalBuildCacheUrl
         if (!localBuildCacheUrl.isNullOrEmpty()) {
             println("Using local build cache located at: $localBuildCacheUrl")
-            local(HttpBuildCache::class.java) {
-                url = URI(localBuildCacheUrl)
+            local(DirectoryBuildCache::class.java) {
+                uri(localBuildCacheUrl)
             }
         }
     }
